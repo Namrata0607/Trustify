94d51b31dcade239919ae051d3f02f2c0bff3224
Namrata0607 <namratadaphale07@gmail.com>
2025-09-15 04:10:10 +0530

    Fixing vercel.json

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import morgan from "morgan";
-import cors from "cors";
 
 import authRoutes from "./routes/authRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
